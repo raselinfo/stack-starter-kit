2c479df499c5f5d1060ae03ee423d0661392a699
raselinfo <raselinfo52@gmail.com>
2023-11-28 23:24:14 +0600

    feat(user): add login functionality
    This commit adds the ability for users to log in to the application.
    
    This commit also fixes a bug with the registration form.
    
    feat issue #123

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,6 @@ import config from 'config';
 import { server } from '@app/app';
 
 const PORT = config.get('port');
-
-
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`http://localhost:${PORT}`);
